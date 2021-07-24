4d42f65515c7f5d5ebcd8ed41764a9d8f8c8022f
evansemet <82794849+evansemet@users.noreply.github.com>
2021-07-24 02:25:21 -0400

    Time: 4 ms (88.14%), Space: 5.9 MB (27.68%) - LeetHub

---
+++ rectangle-area/rectangle-area.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int leftX(max(ax1, bx1));
+        int rightX(min(ax2, bx2));
+        int leftY(max(ay1, by1));
+        int rightY(min(ay2, by2));
+        int shared;
+        if (rightX <= leftX || rightY <= leftY) {
+            shared = 0;
+        } else {
+            shared = (rightX - leftX) * (rightY - leftY);
+        }
+        return (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1) - shared;
+    }
+};
