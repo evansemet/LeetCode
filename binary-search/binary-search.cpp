82aa68ae84ebb47a1f07ad3a96097bf51fd8cae1
evansemet <82794849+evansemet@users.noreply.github.com>
2021-11-25 18:17:34 -0500

    Time: 65 ms (9.70%), Space: 27.6 MB (63.00%) - LeetHub

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -3,14 +3,10 @@ public:
     int search(vector<int>& nums, int target) {
         int left(0), right(nums.size() - 1);
         while (left <= right) {
-            int center = left + (right - left) / 2;
-            if (nums[center] < target) {
-                left = center + 1;
-            } else if (nums[center] > target) {
-                right = center - 1;
-            } else {
-                return center;
-            }
+            int center((right - left) / 2 + left);
+            if (nums[center] == target) return center;
+            else if (nums[center] < target) left = center + 1;
+            else right = center - 1;
         }
         return -1;
     }
