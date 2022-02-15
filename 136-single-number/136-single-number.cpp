3edb68c57d018a91a9a400cd2c9d8894a787b048
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-14 21:13:05 -0600

    Time: 19 ms (78.37%), Space: 16.9 MB (85.89%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int answer = 0;
+        for (int &number: nums) {
+            answer ^= number;
+        }
+        return answer;
+    }
+};
