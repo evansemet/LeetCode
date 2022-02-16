b33b8691a2614e340950fb8185b54a82a9e47097
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-16 14:45:17 -0600

    Time: 0 ms (100.00%), Space: 5.8 MB (90.55%) - LeetHub

--- 507-perfect-number/507-perfect-number.cpp
+++ 507-perfect-number/507-perfect-number.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int sum = 0;
-        for (int i = 1; i <= num / 2; ++i) {
+        for (int i = 1; i * i < num; ++i) {
             if (num % i == 0) {
                 sum += i;
+                if (i != 1) {
+                    sum += (num / i);
+                }
             }
             if (sum > num) {
                 return false;
