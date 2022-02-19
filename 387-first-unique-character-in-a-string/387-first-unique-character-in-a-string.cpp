d932b7a486b53f72d0efbf3b0abb79e928cbf892
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-19 12:10:51 -0600

    Time: 61 ms (35.06%), Space: 10.7 MB (42.49%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        unordered_map<char, int> check;
+        for (char &letter: s) {
+            ++check[letter];
+        }
+        
+        for (int i = 0; i < s.size(); ++i) {
+            if (check[s[i]] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
