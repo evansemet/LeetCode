899e28ff813e39d0f0f0f65b0b62fe333673770d
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-21 03:47:20 -0600

    Time: 16 ms (57.95%), Space: 10.1 MB (12.40%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -7,17 +7,12 @@ public:
             int left = i + 1, right = nums.size() - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum == target) {
-                    return target;
-                }
+                if (sum == target) return target;
                 if (abs(target - sum) < abs(diff)) {
                     diff = target - sum;
                 }
-                if (sum < target) {
-                    ++left;
-                } else {
-                    --right;
-                }
+                if (sum < target) ++left;
+                else --right;
             }
         }
         return target - diff;
