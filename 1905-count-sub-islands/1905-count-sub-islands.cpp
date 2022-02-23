52a9dec2ccc0ce9029a75bf0c14e6c1216b543e2
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-22 19:35:57 -0600

    Time: 508 ms (42.69%), Space: 98.7 MB (29.59%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int ROWS = grid1.size(), COLS = grid1[0].size(), count = 0;
-        vector<vector<int>> visited(grid1.size(), vector<int>(grid1[0].size(), 0));
+        vector<vector<int>> visited(ROWS, vector<int>(COLS, 0));
         for (int i = 0; i < ROWS; ++i) {
             for (int j = 0; j < COLS; ++j) {
                 if (grid2[i][j] && !visited[i][j] && dfs(grid1, grid2, i, j, visited)) {
@@ -13,10 +13,9 @@ public:
         return count;
     }
     
-    
     bool dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j, vector<vector<int>>& visited) {
-        if (i < 0 || j < 0 || i == grid1.size() || j == grid1[0].size() || 
-           !grid2[i][j] || visited[i][j]) {
+        int ROWS = grid1.size(), COLS = grid1[0].size();
+        if (i < 0 || j < 0 || i == ROWS || j == COLS || !grid2[i][j] || visited[i][j]) {
             return true;
         }
         visited[i][j] = 1;
