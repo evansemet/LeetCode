1ced624ecd89d9fef14b2e42ade0cab051f2a487
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-23 12:53:01 -0600

    Time: 0 ms (100.00%), Space: 6 MB (39.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        int first = 0, second = 1, third;
         if (n < 2) return n;
+        int first = 0, second = 1, third;
         for (int i = 2; i <= n; ++i) {
             third = first + second;
             first = second;
