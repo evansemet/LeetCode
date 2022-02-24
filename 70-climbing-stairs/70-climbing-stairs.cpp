bfbd3f526c808df5c001aaa164bd6ba6b599109e
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-24 13:43:48 -0600

    Time: 0 ms (100.00%), Space: 6.2 MB (28.96%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n < 2) return n;
-        int first = 1, second = 2;
-        for (int i = 3; i <= n; ++i) {
-            int third = first + second;
-            first = second;
-            second = third;
+        vector<int> dp(n + 1, 1);
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return second;
+        return dp[n];
     }
 };
