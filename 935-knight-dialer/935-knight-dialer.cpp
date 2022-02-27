b63464f83b5591e563a3d30277485e64fee2b278
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-26 18:03:10 -0600

    Time: 139 ms (69.89%), Space: 43.8 MB (27.79%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.cpp
+++ 935-knight-dialer/935-knight-dialer.cpp
@@ -1,16 +1,23 @@
-class Solution:
-    def knightDialer(self, n: int) -> int:
-        arr = [1] * 10
-        for _ in range(n - 1):
-            dp = [0] * 10
-            dp[0] = arr[4] + arr[6]
-            dp[1] = arr[6] + arr[8]
-            dp[2] = arr[7] + arr[9]
-            dp[3] = arr[4] + arr[8]
-            dp[4] = arr[0] + arr[3] + arr[9]
-            dp[6] = arr[0] + arr[1] + arr[7]
-            dp[7] = arr[2] + arr[6]
-            dp[8] = arr[1] + arr[3]
-            dp[9] = arr[2] + arr[4]
-            arr = dp
-        return sum(arr) % (10 ** 9 + 7)
+class Solution {
+public:
+    int knightDialer(int n) {
+        int mod = 1000000000 + 7;
+        vector<long int> arr(10, 1);
+        for (int i = 1; i <  n; ++i) {
+            vector<long int> dp(10, 0);
+            dp[0] = (arr[4] + arr[6]) % mod;
+            dp[1] = (arr[6] + arr[8]) % mod;
+            dp[2] = (arr[7] + arr[9]) % mod;
+            dp[3] = (arr[4] + arr[8]) % mod;
+            dp[4] = (arr[0] + arr[3] + arr[9]) % mod;
+            dp[6] = (arr[0] + arr[1] + arr[7]) % mod;
+            dp[7] = (arr[2] + arr[6]) % mod;
+            dp[8] = (arr[1] + arr[3]) % mod;
+            dp[9] = (arr[2] + arr[4]) % mod;
+            arr = dp;
+        }
+        long int sum = 0;
+        for (long int num : arr) sum += num;
+        return sum % mod;
+    }
+};
