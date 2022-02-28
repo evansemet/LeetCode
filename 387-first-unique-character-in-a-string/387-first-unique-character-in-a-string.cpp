cdb90c96f9791617d2e9a218e1ad396492481ce2
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-28 17:21:10 -0600

    Time: 24 ms (90.42%), Space: 10.6 MB (70.43%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        
-        unordered_map<char, size_t> check;
-        for (char &letter : s) ++check[letter];
-        
-        for (int i = 0; i < s.size(); ++i) {
-            if (check[s[i]] == 1) {
-                return i;
-            }
-        }
-        
+        vector<int> count(26);
+        for (char &let : s) ++count[let - 'a'];
+        for (int i = 0; i < s.size(); ++i) if (count[s[i] - 'a'] == 1) return i;
         return -1;
     }
 };
