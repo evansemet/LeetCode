6fe069c2ebc95fccd3c9c7e36d77c26610382078
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-02-28 13:28:50 -0600

    Time: 29 ms (33.43%), Space: 8.9 MB (16.11%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         if (ransomNote.size() > magazine.size()) return false;
-        unordered_map<char, int> m, r;
+        unordered_map<char, int> m;
         for (char let : magazine) ++m[let];
         for (char let : ransomNote) {
-            ++r[let];
-            if (r[let] > m[let]) return false;
+            --m[let];
+            if (m[let] < 0) return false;
         }
         return true;
     }
