6337eecf47a2ea26f1cef13e3757f7ece0db1fdd
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-07-20 16:49:51 -0400

    Time: 52 ms (54.10%), Space: 13.9 MB (13.75%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,13 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         seen = []
-        n = str(n)
-        while n != "1":
-            nxt = 0
-            for c in n:
-                nxt += int(c) ** 2
-            n = str(nxt)
-            if n in seen:
+        
+        def next(n):
+            x = sum(int(dig)**2 for dig in str(n))
+            if x == 1:
+                return True
+            elif x in seen:
                 return False
-            seen.append(n)
-        return True
+            else:
+                seen.append(x)
+                return next(x)
+        
+        return next(n)
