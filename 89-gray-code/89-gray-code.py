9804d7e4d7e6ba0d29e12a07d66d87508bbd4a5c
Evan Semet <82794849+evansemet@users.noreply.github.com>
2022-07-22 15:44:13 -0400

    Time: 250 ms (18.87%), Space: 21.4 MB (70.89%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -1,7 +1,6 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
         res = []
-        seqLen = 1 << n
-        for i in range(seqLen):
+        for i in range(1 << n):
             res.append(i ^ i >> 1)
         return res
